bf7b9aa9d4e605fc6947827a88ba42c6a42cd3c8
web-bombaster <skorpion_05@inbox.ru>
2023-10-27 15:24:55 +0300

    футер и заявка

--- src/js/myscripts/header.js
+++ src/js/myscripts/header.js
@@ -1,15 +1,31 @@
-const headerFixed = function() {
-    if (document.querySelector('.header')) {
-        const header = document.querySelector('.header');
-        const heightHeader = header.offsetHeight; // высота хедера
-        
-        let padTop = 0;
-        padTop = parseInt(window.getComputedStyle(header.parentElement, null).getPropertyValue('padding-top')); // padding-top родителя
-        
-        header.parentElement.style.paddingTop = heightHeader + padTop + 'px'; // даем родителю нужный отступ
-    };
+const headerFixed = function () {
+	if (document.querySelector(".header")) {
+		const header = document.querySelector(".header");
+
+		let heightHeader = header.offsetHeight; // высота хедера
+
+
+		header.parentElement.style.paddingTop = heightHeader + "px"; // даем родителю нужный отступ
+	}
 };
 
 headerFixed();
 // запускаем headerFixed при ресайзе
 window.addEventListener("resize", headerFixed);
+
+// при прокрутке даем хедеру клаасс toogle для фиксации
+const headerToggle = function () {
+	if (document.querySelector(".header")) {
+		const header = document.querySelector(".header");
+
+		if (window.scrollY > 0) {
+			header.classList.add("toogle");
+		} else {
+            header.classList.remove("toogle");
+        };
+	}
+};
+
+headerToggle();
+// запускаем headerFixed при ресайзе
+window.addEventListener("scroll", headerToggle);
--- src/js/myscripts/moving.js
+++ src/js/myscripts/moving.js
@@ -5,10 +5,25 @@ let moving = function () {
     const windowWidth = window.innerWidth; // ширина экрана
     // console.log(windowWidth);
 
+    // перемещение меню
     if (windowWidth <= 600) {
         document.querySelector('.mobile-menu__wrapper').prepend(document.querySelector('.header-menu'));
     } else {
         document.querySelector('.header-nav').append(document.querySelector('.header-menu'));
+    };
+
+    // перемещение копирайта
+    if (windowWidth <= 600) {
+        document.querySelector('.footer-col__03').append(document.querySelector('.footer-box__copy'));
+    } else {
+        document.querySelector('.footer-col__01').append(document.querySelector('.footer-box__copy'));
+    };
+
+    // перемещение footer-contacts
+    if (windowWidth <= 600) {
+        document.querySelector('.footer-col__01').append(document.querySelector('.footer-contacts'));
+    } else {
+        document.querySelector('.footer-col__03').prepend(document.querySelector('.footer-contacts'));
     };
 };
 
